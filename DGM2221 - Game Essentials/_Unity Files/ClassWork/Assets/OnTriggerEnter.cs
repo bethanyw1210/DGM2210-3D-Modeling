696f6a7594459c26a980b3cd8b050e56be661f91
Bethany Woodruff <bethany.w1210@gmail.com>
2020-10-19 19:26:57 -0600

    combined 3D modeling and game essentials classes

---
+++ DGM2221 - Game Essentials/_Unity Files/ClassWork/Assets/OnTriggerEnter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+
+public class OnTriggerEnter : MonoBehaviour
+{
+    public Color newColor;
+    public Color defaultColor;
+    private MeshRenderer meshR;
+    private WaitForSeconds wfs;
+    public int holdTime = 1;
+
+    void Start()
+    {
+        meshR = GetComponent<MeshRenderer>();
+        meshR.material.color = defaultColor;
+        wfs = new WaitForSeconds(1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        meshR.material.color = newColor;
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        yield return wfs;
+        meshR.material.color = defaultColor;
+    }
+}
---
+++ DGM2221 - Game Essentials/_Unity Files/Pogo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+
+    public float rotateSpeed = 300f, gravity = 1f, maxFlyCount = 5f, flyHeight = 15f;
+    private float yVar, flyCount;
+
+    public FloatData normalSpeed, sprintSpeed;
+    private FloatData moveSpeed;
+    private bool canMove = true;
+
+    private void Start()
+    {
+        moveSpeed = normalSpeed;
+        controller = GetComponent<CharacterController>();
+        StartCoroutine(Move());
+    }
+    
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private IEnumerator Move()
+    {
+        canMove = true;
+        while (canMove)
+        {
+            yield return wffu;
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                moveSpeed = sprintSpeed;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                moveSpeed = normalSpeed;
+            }
+        
+            var vInput = Input.GetAxis("Vertical")*moveSpeed.value;
+            
+            movement.Set(vInput,yVar,0);
+        
+            var hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
+            transform.Rotate(0,hInput,0);
+
+            yVar += gravity*Time.deltaTime;
+            
+            if (controller.isGrounded && movement.y < 0)
+            {
+                yVar = -1f;
+                flyCount = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && flyCount < maxFlyCount)
+            {
+                yVar = flyHeight;
+                flyCount++;
+            }
+
+            movement = transform.TransformDirection(movement);
+            controller.Move((movement) * Time.deltaTime);
+        }
+    }
+}
