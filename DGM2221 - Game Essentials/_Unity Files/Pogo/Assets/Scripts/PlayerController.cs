c076ad594a6e60f6de04b056d81c1ff01824d881
Bethany Woodruff <bethany.w1210@gmail.com>
2020-10-20 17:34:07 -0600

    DGM2221 - tweaking character movement some more

--- DGM2221 - Game Essentials/_Unity Files/Pogo/Assets/Scripts/PlayerController.cs
+++ DGM2221 - Game Essentials/_Unity Files/Pogo/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public FloatData normalSpeed, sprintSpeed;
+    public IntData flightAmount;
+    public float rotateSpeed = 300f, gravity = 1f, flyHeight = 15f;
+    
     private CharacterController controller;
     private Vector3 movement;
-
-    public float rotateSpeed = 300f, gravity = 1f, maxFlyCount = 5f, flyHeight = 15f;
-    private float yVar, flyCount;
-
-    public FloatData normalSpeed, sprintSpeed;
+    private float vInput, hInput, yVar, flyCount;
     private FloatData moveSpeed;
     private bool canMove = true;
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
 
     private void Start()
     {
@@ -24,46 +25,52 @@ public class PlayerController : MonoBehaviour
         StartCoroutine(Move());
     }
     
-    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
     private IEnumerator Move()
     {
         canMove = true;
         while (canMove)
         {
+            OnHorizontal();
+            OnVertical();
             yield return wffu;
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                moveSpeed = sprintSpeed;
-            }
-
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                moveSpeed = normalSpeed;
-            }
-        
-            var vInput = Input.GetAxis("Vertical")*moveSpeed.value;
-            
-            movement.Set(vInput,yVar,0);
-        
-            var hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
-            transform.Rotate(0,hInput,0);
-
-            yVar += gravity*Time.deltaTime;
-            
-            if (controller.isGrounded && movement.y < 0)
-            {
-                yVar = -1f;
-                flyCount = 0;
-            }
-
-            if (Input.GetButtonDown("Jump") && flyCount < maxFlyCount)
-            {
-                yVar = flyHeight;
-                flyCount++;
-            }
-
-            movement = transform.TransformDirection(movement);
-            controller.Move((movement) * Time.deltaTime);
         }
+    }
+    
+    private void OnHorizontal()
+    {
+        hInput = Input.GetAxis("Vertical")*Time.deltaTime*rotateSpeed;
+        transform.Rotate(0,-hInput,0);
+    }
+    
+    private void OnVertical()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = sprintSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = normalSpeed;
+        }
+        
+        vInput = Input.GetAxis("Horizontal")*moveSpeed.value;
+        movement.Set(vInput,yVar,hInput);
+
+        yVar += gravity*Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            yVar = -1f;
+            flyCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && flyCount < flightAmount.value)
+        {
+            yVar = flyHeight;
+            flyHeight++;
+        }
+        movement = transform.TransformDirection(movement);
+        controller.Move((movement) * Time.deltaTime);
     }
 }
