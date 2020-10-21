fa3356673d260068f5cb2878afeec74d020a6687
Bethany Woodruff <bethany.w1210@gmail.com>
2020-10-21 14:21:50 -0600

    DGM2670 - Switches and wall crawl

---
+++ DGM2670 - Scripting II/DGM2670/Assets/Scripts/CharacterStateMachineData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+
+public class CharacterStateMachineData : ScriptableObject
+{
+    public enum characterStates
+    {
+        StandardWalk, 
+        WallCrawl,
+        Knockback,
+        NoGravityWalk
+    }
+
+    public characterStates value;
+
+    public UnityEvent onEnableEvent;
+
+    public void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
+
+    public void StandardWalk()
+    {
+        value = characterStates.StandardWalk;
+    }
+    
+    public void WallCrawl()
+    {
+        value = characterStates.WallCrawl;
+    }
+    
+    public void Knockback()
+    {
+        value = characterStates.Knockback;
+    }
+}
---
+++ DGM2670 - Scripting II/DGM2670/Assets/Scripts/CharacterWithStates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterWithStates : MonoBehaviour
+{
+    private CharacterController controller;
+    public CharacterStateMachineData characterStates;
+
+    private Vector3 movement;
+    public float moveSpeed = 3f, gravity = -9.81f;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var newInput = Input.GetAxis("Vertical")*moveSpeed;
+        
+        switch (characterStates.value)
+        {
+            case CharacterStateMachineData.characterStates.StandardWalk:
+                movement.Set(newInput,gravity,0);
+                print("Normal walk");
+                break;
+            case CharacterStateMachineData.characterStates.NoGravityWalk:
+                movement.Set(newInput,0,0);
+                print("Normal walk");
+                break;
+            case CharacterStateMachineData.characterStates.WallCrawl:
+                movement.Set(0,newInput,0);
+                print("Wall crawl");
+                break;
+            case CharacterStateMachineData.characterStates.Knockback:
+                print("Knockback");
+                break;
+        }
+
+        var newMovement = moveSpeed * Time.deltaTime;
+        controller.Move(newMovement);
+    }
+}
