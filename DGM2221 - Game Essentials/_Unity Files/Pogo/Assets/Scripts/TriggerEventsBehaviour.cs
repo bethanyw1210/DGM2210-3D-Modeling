87f5966a921fc4495fefef659714bae459a5af9b
Bethany Woodruff <bethany.w1210@gmail.com>
2020-10-22 15:53:34 -0600

    DGM2221 - Beefing up trigger events script

--- DGM2221 - Game Essentials/_Unity Files/Pogo/Assets/Scripts/TriggerEventsBehaviour.cs
+++ DGM2221 - Game Essentials/_Unity Files/Pogo/Assets/Scripts/TriggerEventsBehaviour.cs
@@ -6,10 +6,25 @@ using UnityEngine.Events;
 
 public class TriggerEventsBehaviour : MonoBehaviour
 {
-    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerEnterEvent, triggerExitEvent,triggerStayEvent;
+    public float enterDelayTime = 0.01f, exitDelayTime = 0.01f, stayDelayTime = 0.01f;
+    
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
+        yield return new WaitForSeconds(enterDelayTime);
         triggerEnterEvent.Invoke();
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        yield return new WaitForSeconds(exitDelayTime);
+        triggerExitEvent.Invoke();
+    }
+
+    private IEnumerator OnTriggerStay(Collider other)
+    {
+        yield return new WaitForSeconds(stayDelayTime);
+        triggerStayEvent.Invoke();
     }
 }
