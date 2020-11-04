78cb12923bd0aa791b3406ebb351e9fdaa1bd3de
Bethany Woodruff <bethany.w1210@gmail.com>
2020-11-04 14:25:01 -0700

    DGM2670 - anims with state machines

---
+++ DGM2670 - Scripting II/DGM2670/Assets/Scripts/Behaviors/CharacterMoveStates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterMoveStates : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    public float speed = 5;
+    public CharacterBrain brain;
+    private CharacterController controller;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        startEvent.Invoke();
+    }
+    private void Update()
+    {       
+        var position = brain.Move(speed*Input.GetAxis("Horizontal"));
+        var newPosition = position * Time.deltaTime;
+        controller.Move(newPosition);
+    }
+}
---
+++ DGM2670 - Scripting II/DGM2670/Assets/Scripts/CharacterBrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class CharacterBrain : ScriptableObject
+{
+    public Vector3Data position;
+    public Vector3 Move( float speed)
+    {
+        var newValue = position.value * speed;
+        return newValue;
+    }
+    public void Change(Vector3Data data)
+    {
+        position = data;
+    }
+}
