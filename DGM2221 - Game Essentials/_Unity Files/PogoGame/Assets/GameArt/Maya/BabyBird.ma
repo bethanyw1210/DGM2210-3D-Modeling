c17b1e2b9735b14660fdb2ca896de49197f6740a
Bethany Woodruff <bethany.w1210@gmail.com>
2020-11-21 19:42:39 -0700

    DGM2221 - bird cage destroyed and baby bird sets as child to pogo

---
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/GameArt/Maya/BabyBird.ma
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:40e5d3d403cca340f84caff40d473a12a7ac725365d0a4069b5fcfe33d41f46e
+size 61467
---
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/RandomSpawnLocation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnLocation : MonoBehaviour
+{
+    public float minX = -31f, minY = 4f, minZ = 1.5f;
+    public float maxX = -33f, maxY = 6f, maxZ = 5.5f;
+
+    public GameObject obj;
+
+    public void SpawnObj()
+    {
+        float x = Random.Range(minX, maxX);
+        float y = Random.Range(minY, maxY);
+        float z = Random.Range(minZ, maxZ);
+
+        Instantiate(obj, new Vector3(x, y, z), Quaternion.identity);
+    }
+}
+ 
