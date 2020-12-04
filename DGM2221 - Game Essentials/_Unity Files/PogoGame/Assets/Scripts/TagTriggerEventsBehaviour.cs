5f962b6c7f18d017b39c743d4956594635c30376
Bethany Ylst <bethany.w1210@gmail.com>
2020-12-04 12:24:41 -0700

    DGM2221 - adding disappearing platforms

--- DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/TagTriggerEventsBehaviour.cs
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/TagTriggerEventsBehaviour.cs
@@ -1,19 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class TagTriggerEventsBehaviour : MonoBehaviour
 {
-    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerEnterEvent, triggerExitEvent;
     public string compareTag;
+    public float enterDelayTime = 0.01f, exitDelayTime = 0.01f;
 
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(compareTag))
         {
+            yield return new WaitForSeconds(enterDelayTime);
             triggerEnterEvent.Invoke();
+        }
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(compareTag))
+        {
+            yield return new WaitForSeconds(exitDelayTime);
+            triggerExitEvent.Invoke();
         }
     }
 }
