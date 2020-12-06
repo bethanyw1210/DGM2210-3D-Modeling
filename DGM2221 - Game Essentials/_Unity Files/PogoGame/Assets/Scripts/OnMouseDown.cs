fba8421e0432c0e4d4c8c0607dd8b717aa5e5906
Bethany Ylst <bethany.w1210@gmail.com>
2020-12-06 14:01:31 -0700

    DGM2221 - fixed weapon spawnpoint errors

---
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/OnMouseDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnMouseDown : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDownEvent.Invoke();
+        }
+    }
+}
