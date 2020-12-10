d480eab6943a875b52f2f22583c54532b831ed4c
Bethany Ylst <bethany.w1210@gmail.com>
2020-12-09 23:05:40 -0700

    DGM2221 - can't shoot infinitely now boiiiii

--- DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/InstanceBehaviour.cs
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/InstanceBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,28 @@ public class InstanceBehaviour : MonoBehaviour
     public GameObject prefab;
     public Vector3Data rotationDirection;
     public GameObject newObj;
+    public bool canShoot;
+    public WaitForSeconds wfs = new WaitForSeconds(.85f);
+
+    public void FireWeapon()
+    {
+        if (canShoot)
+        {
+            Instance();
+        }
+    }
 
     public void Instance()
     {
         var location = transform.position;
         newObj = Instantiate(prefab, location, Quaternion.Euler(rotationDirection.value));
+        StartCoroutine(HoldTime());
+    }
+
+    public IEnumerator HoldTime()
+    {
+        canShoot = false;
+        yield return wfs;
+        canShoot = true;
     }
 }
