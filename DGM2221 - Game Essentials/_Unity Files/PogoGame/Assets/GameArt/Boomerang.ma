9522e178eb0596a19c8895fa21669af1736b6d17
Bethany Ylst <bethany.w1210@gmail.com>
2020-12-11 21:10:51 -0700

    DGM2221 - fixed branch and started on UI

---
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/GameArt/Boomerang.ma
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e3c04ba154018c923749818ba100de28d4bb10e0902b9dc7c896a1216a71e05
+size 55227
---
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class LoadLevel : ScriptableObject
+{
+    public string level;
+
+    public void LoadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }
+}
