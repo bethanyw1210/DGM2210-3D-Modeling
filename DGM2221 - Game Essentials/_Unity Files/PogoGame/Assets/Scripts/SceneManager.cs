f1c5ef24793151c9470b3e7f4bd0a3128f5f71f8
Bethany Ylst <bethany.w1210@gmail.com>
2020-12-11 22:19:10 -0700

    DGM2221 - added more UI, fixed shooting in boss building

--- DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/SceneManager.cs
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/SceneManager.cs
@@ -6,16 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    public string loadLevel;
+    public string gameOver, playerWon, notEnough;
 
-    public FloatData playerHealth;
+    public FloatData playerHealth, bossHealth;
     public IntData birdCount;
 
     public void Update()
     {
-        if (playerHealth.value <= 0 && (birdCount.value <= 0))
+        if (playerHealth.value <= 0 && (birdCount.value <= 0) && (bossHealth.value > 0))
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(loadLevel);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(gameOver);
+        }
+
+        if (bossHealth.value <= 0 && (birdCount.value >= 3) && (playerHealth.value > 0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(playerWon);
+        }
+        
+        if (bossHealth.value <= 0 && (birdCount.value < 3) && (playerHealth.value > 0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(notEnough);
         }
     }
 }
