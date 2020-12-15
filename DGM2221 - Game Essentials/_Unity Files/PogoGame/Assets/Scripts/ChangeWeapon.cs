e45b8d9292743d5a2af85574a64590afb6102a93
Bethany Ylst <bethany.w1210@gmail.com>
2020-12-15 12:43:28 -0700

    DGM2221 - making adjustments to balancing

--- DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/ChangeWeapon.cs
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/ChangeWeapon.cs
@@ -12,11 +12,17 @@ public class ChangeWeapon : MonoBehaviour
     public float newWeaponDmg;
     public GameObject weapon, chest;
     public Material startColor;
+    public FloatData playerHealth;
+    public float newHealth;
+    public GameObject healthBar, healthBarBG;
     
     public void Start()
     {
         weapon.GetComponent<MeshRenderer>().material = startColor;
         originalWeaponDmg.value = .05f;
+        healthBar.SetActive(false);
+        healthBarBG.SetActive(false);
+        playerHealth.value = 1;
     }
     
     private void OnTriggerEnter(Collider other)
@@ -24,6 +30,9 @@ public class ChangeWeapon : MonoBehaviour
         if (other.CompareTag(compareTag))
         {
             originalWeaponDmg.value = newWeaponDmg;
+            playerHealth.value = newHealth;
+            healthBar.SetActive(true);
+            healthBarBG.SetActive(true);
             changeWeaponEvent.Invoke();
             Destroy(chest);
         }
