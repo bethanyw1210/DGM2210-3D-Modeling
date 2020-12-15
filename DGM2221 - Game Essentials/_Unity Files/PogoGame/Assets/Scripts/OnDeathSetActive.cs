b9e346084c46dffdbf1a6403abd446545fa3a5e6
Bethany Ylst <bethany.w1210@gmail.com>
2020-12-15 16:53:12 -0700

    dgm2221 - playtest adjustments 2

--- DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/OnDeathSetActive.cs
+++ DGM2221 - Game Essentials/_Unity Files/PogoGame/Assets/Scripts/OnDeathSetActive.cs
@@ -6,22 +6,20 @@ using UnityEngine;
 public class OnDeathSetActive : MonoBehaviour
 {
     public FloatData enemyHealth;
-    public GameObject objInstantiate, objInstantiate2, objInstantiate3;
+    public GameObject objInstantiate, objInstantiate2;
 
     public void Start()
     {
         objInstantiate.SetActive(false);
         objInstantiate2.SetActive(false);
-        objInstantiate3.SetActive(false);
     }
 
     public void Update()
     {
-        if (enemyHealth.value <= 0)
+        if (enemyHealth.value <= .05)
         {
             objInstantiate.SetActive(true);
             objInstantiate2.SetActive(true);
-            objInstantiate3.SetActive(true);
         }
     }
 }
